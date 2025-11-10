988c4d2daf927cc6cb6c84685827abdf1059a04b
Simon Z <143943099+simon-zha@users.noreply.github.com>
2025-11-11 00:06:31 +1100

    Update docker_images.tf

--- docker_images.tf
+++ docker_images.tf
@@ -20,7 +20,7 @@ module "docker_image_analytics_lambda" {
   source = "terraform-aws-modules/lambda/aws//modules/docker-build"
 
   create_ecr_repo = true
-  ecr_repo        = "sbeacon-analytics-lambda-containers"
+  ecr_repo        = "${local.environment_prefix}-analytics-lambda-containers"
   ecr_repo_lifecycle_policy = jsonencode({
     "rules" : [
       {
@@ -57,7 +57,7 @@ module "docker_image_askbeacon_lambda" {
   source = "terraform-aws-modules/lambda/aws//modules/docker-build"
 
   create_ecr_repo = true
-  ecr_repo        = "sbeacon-askbeacon-lambda-containers"
+  ecr_repo        = "${local.environment_prefix}-askbeacon-lambda-containers"
   ecr_repo_lifecycle_policy = jsonencode({
     "rules" : [
       {
