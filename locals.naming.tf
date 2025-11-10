ae2d65c07ca9a5c9aa037da4a8d774fcf26f0865
Simon Z <143943099+simon-zha@users.noreply.github.com>
2025-11-11 01:37:41 +1100

    Update locals.naming.tf

--- locals.naming.tf
+++ locals.naming.tf
@@ -1,15 +1,14 @@
+# ../locals.naming.tf 
 locals {
-  environment_slug_initial = regexreplace(lower(var["beacon-environment"]), "[^0-9a-z-]", "-")
-  environment_slug_collapsed = regexreplace(local.environment_slug_initial, "-{2,}", "-")
-  environment_slug_trimmed_head = regexreplace(local.environment_slug_collapsed, "^-", "")
-  environment_slug_trimmed = regexreplace(local.environment_slug_trimmed_head, "-$", "")
-  environment_slug = length(local.environment_slug_trimmed) > 0 ? local.environment_slug_trimmed : "env"
+  env_in   = lower(trimspace(coalesce(var.beacon_environment, terraform.workspace)))
+
+  env_map  = { dev = "dev", staging = "staging", prod = "prod" }
+  environment_slug = lookup(local.env_map, local.env_in, "dev")
 
   environment_snake     = replace(local.environment_slug, "-", "_")
   environment_suffix    = "-${local.environment_slug}"
   environment_suffix_sn = "_${local.environment_snake}"
   environment_suffix_up = "-${upper(local.environment_slug)}"
-  environment_suffix_no = "${local.environment_slug}"
+  environment_suffix_no = local.environment_slug
   environment_prefix    = "sbeacon-${local.environment_slug}"
 }
-
