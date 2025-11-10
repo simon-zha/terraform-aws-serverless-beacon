dc4cec21e427649d47f126bd449ba8c85907080d
Simon Z <143943099+simon-zha@users.noreply.github.com>
2025-11-11 00:02:09 +1100

    Create locals.naming.tf

---
+++ locals.naming.tf
@@ -0,0 +1,15 @@
+locals {
+  environment_slug_initial = regexreplace(lower(var.beacon-environment), "[^0-9a-z-]", "-")
+  environment_slug_collapsed = regexreplace(local.environment_slug_initial, "-{2,}", "-")
+  environment_slug_trimmed_head = regexreplace(local.environment_slug_collapsed, "^-", "")
+  environment_slug_trimmed = regexreplace(local.environment_slug_trimmed_head, "-$", "")
+  environment_slug = length(local.environment_slug_trimmed) > 0 ? local.environment_slug_trimmed : "env"
+
+  environment_snake     = replace(local.environment_slug, "-", "_")
+  environment_suffix    = "-${local.environment_slug}"
+  environment_suffix_sn = "_${local.environment_snake}"
+  environment_suffix_up = "-${upper(local.environment_slug)}"
+  environment_suffix_no = "${local.environment_slug}"
+  environment_prefix    = "sbeacon-${local.environment_slug}"
+}
+
