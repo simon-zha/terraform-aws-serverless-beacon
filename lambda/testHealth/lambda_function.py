49b284d387900dfb6686b0d1d6d7fda19d32bd93
Simon Z <143943099+simon-zha@users.noreply.github.com>
2025-11-17 16:25:02 +1100

    Update lambda_function.py

--- lambda/testHealth/lambda_function.py
+++ lambda/testHealth/lambda_function.py
@@ -7,7 +7,7 @@ def lambda_handler(event, context):
     "headers": {"Content-Type": "application/json"},
     "body": json.dumps(
       {
-        "message": "this is commit",
+        "message": "this is commit for test",
         "path": event.get("path"),
         "environment": event.get("requestContext", {})
           .get("stage")
